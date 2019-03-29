e5e1550447a820e45f2fd66befabb2054f80204b
Tristan Behrens <tbehrens@jupyterhub.kuyuptzi2ituth0si2far3yj1h.ax.internal.cloudapp.net>
2019-03-29 09:25:23 +0000

    Syncing the CSV now works. Refactoring.

--- cgm_database/dbutils.py
+++ cgm_database/dbutils.py
@@ -14,7 +14,7 @@ def connect_to_default_database():
     )
     connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     connection.autocommit = True
-    return connection
+    return DatabaseInterface(connection)
     
     
 def connect_to_main_database():
@@ -29,7 +29,7 @@ def connect_to_main_database():
     )
     connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     connection.autocommit = True
-    return connection
+    return DatabaseInterface(connection)
     
     
 def load_dbconnection_file():
@@ -37,3 +37,32 @@ def load_dbconnection_file():
         json_data = json.load(json_file)
         return json_data
 
+    
+class DatabaseInterface:
+    
+    def __init__(self, connection):
+        self.connection = connection
+        self.cursor = connection.cursor()
+        
+    def execute(self, script):
+        self.cursor.execute(script)
+        self.connection.commit()
+        
+    def execute_script_file(self, filename):
+        self.cursor.execute(open(filename, "r").read())
+        self.connection.commit()
+    
+    def get_all_tables(self):
+        self.cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+        tables = [str(table[0]) for table in self.cursor.fetchall()]
+        return tables
+    
+    def clear_table(self, table):
+        result = self.cursor.execute("TRUNCATE TABLE {};".format(table))
+        self.connection.commit()
+
+    def get_number_of_rows(self, table):
+        self.cursor.execute("SELECT COUNT(*) from {};".format(table))
+        result = self.cursor.fetchone()
+        return result[0]
+    
