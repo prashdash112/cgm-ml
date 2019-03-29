3bfe63759f33488e35d61ffdb45e950f0a02f68a
Tristan Behrens <tbehrens@jupyterhub.kuyuptzi2ituth0si2far3yj1h.ax.internal.cloudapp.net>
2019-03-29 10:34:33 +0000

    Adds tables for PCDs and JPGs.

--- cgm_database/schema.sql
+++ cgm_database/schema.sql
@@ -1,6 +1,8 @@
+-- This removes everything. You better be careful.
 DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;
 
+-- Creates a table for measurements.
 CREATE TABLE IF NOT EXISTS measurements (
     id SERIAL PRIMARY KEY,
     measurement_id TEXT NOT NULL,
@@ -24,3 +26,34 @@ CREATE TABLE IF NOT EXISTS measurements (
     created_by TEXT NOT NULL
 );
 
+-- Creates a table for image data.
+CREATE TABLE IF NOT EXISTS image_data (
+    id SERIAL PRIMARY KEY,
+    path TEXT NOT NULL,
+    qrcode TEXT NOT NULL,
+    targets TEXT NOT NULL,
+    last_updated TIMESTAMP NOT NULL,
+    rejected_by_expert BOOLEAN NOT NULL,
+    had_error BOOLEAN NOT NULL,
+    error_message TEXT,
+    width_px INTEGER NOT NULL,
+    height_px INTEGER NOT NULL,
+    blur_variance REAL NOT NULL
+);
+
+-- Creates a table for pointcloud data.
+CREATE TABLE IF NOT EXISTS pointcloud_data (
+    id SERIAL PRIMARY KEY,
+    path TEXT NOT NULL,
+    qrcode TEXT NOT NULL,
+    targets TEXT NOT NULL,
+    last_updated TIMESTAMP NOT NULL,
+    rejected_by_expert BOOLEAN NOT NULL,
+    had_error BOOLEAN NOT NULL,
+    error_message TEXT,
+    number_of_points INTEGER NOT NULL,  
+    confidence_min REAL NOT NULL,
+    confidence_avg REAL NOT NULL,
+    confidence_std REAL NOT NULL,
+    confidence_max REAL NOT NULL
+);
