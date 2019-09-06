e3edeb4e3655a9e617982e7134a8d94520ccbe47
Markus Matiaschek <mmatiaschek@gmail.com>
2019-09-06 07:54:36 +0000

    improves get_messages.py iteration over messages and logging

--- etl/get_message.py
+++ etl/get_message.py
@@ -29,20 +29,22 @@ def main():
     
     base64_regex = re.compile("^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$")  
     
+    key_file = "{0}/{1}/acc_key.txt".format(destination_folder, storage_account_name)
     try :
-        with open("{0}/{1}/acc_key.txt".format(destination_folder, storage_account_name), "r") as f:
+        with open(key_file, "r") as f:
             account_key = f.read()
     except IOError:
-        print("file not found or empty")
+        print("file not found or empty ", key_file)
     
     queue_service = QueueService(storage_account_name, account_key)
 
     metadata = queue_service.get_queue_metadata(queue_name)
-    total_count = metadata.approximate_message_count
-    count = total_count
-    print('number of messages in queue is ', count)
+    approximate_message_count = metadata.approximate_message_count
+    print('fetching approximately {0} messages'.format(approximate_message_count))
+    messages = queue_service.get_messages(queue_name)
+    count = len(messages)
+    total = 0
     while count > 0:
-        messages = queue_service.get_messages(queue_name)
         for message in messages:
             if base64_regex.match(message.content):
                 msg = base64.b64decode(message.content)     # Received message is encoded in base 64
@@ -59,7 +61,11 @@ def main():
                 json.dump(data, f, indent=2)
             queue_service.delete_message(queue_name, message.id, message.pop_receipt) 
             count -= 1
-            print('successfully processed message, {0} left'.format(count))
+            total += 1
+            print('successfully processed {0} messages of ~{1} from {2} queue {3}'.format(total,approximate_message_count,storage_account_name,queue_name))
+
+        messages = queue_service.get_messages(queue_name)
+        count = len(messages)
             
 
         
