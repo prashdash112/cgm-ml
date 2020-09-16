d1c91316ea08e15388b5f906b2f9bab68743a1ed
Markus Hinsche <m.hinsche@gmail.com>
2020-09-16 13:47:23 +0200

    Rename p to point, make a function

--- depthmap_toolkit/pcd2depth.py
+++ depthmap_toolkit/pcd2depth.py
@@ -3,26 +3,33 @@ import numpy as np
 import utils
 import zipfile
 
+utils.setWidth(int(240 * 0.75))  # 180
+utils.setHeight(int(180 * 0.75))  # 135
+width = utils.getWidth()
+height = utils.getHeight()
 
-def process(calibration, pcd, depthfile):
 
-    # read PCD and calibration
-    calibration = utils.parseCalibration(calibration)
-    points = utils.parsePCD(pcd)
-    utils.setWidth(int(240 * 0.75))
-    utils.setHeight(int(180 * 0.75))
-
-    # convert to depthmap
-    width = utils.getWidth()
-    height = utils.getHeight()
+def convert_to_depthmap(calibration, points):
     output = np.zeros((width, height, 3))
-    for p in points:
-        v = utils.convert3Dto2D(calibration[1], p[0], p[1], p[2])
+    for point in points:
+        v = utils.convert3Dto2D(calibration[1], point[0], point[1], point[2])
         x = int(width - v[0] - 1)
         y = int(height - v[1] - 1)
         if x >= 0 and y >= 0 and x < width and y < height:
-            output[x][y][0] = p[3]
-            output[x][y][2] = p[2]
+            output[x][y][0] = point[3]
+            output[x][y][2] = point[2]
+    return output
+
+    # first channel of output is still zero
+
+def process(calibration_path, pcd, depthfile):
+
+    # read PCD and calibration
+    calibration = utils.parseCalibration(calibration_path)
+    points = utils.parsePCD(pcd)
+
+    # convert to depthmap
+    output = convert_to_depthmap(calibration, points)
 
     # write depthmap
     with open('data', 'wb') as f:
