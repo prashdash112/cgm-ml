ac625f58092a70bd81935e2941ff8ca79a901166
Markus Hinsche <m.hinsche@gmail.com>
2020-10-06 07:32:10 +0200

    Pep8ify depthmap_toolkit (#109)
    Pep8ify depthmap_toolkit

--- src/common/depthmap_toolkit/depthmap.py
+++ src/common/depthmap_toolkit/depthmap.py
@@ -5,156 +5,168 @@ import utils
 import zipfile
 
 #export data
+
+
 def export(type, filename):
-  if type == 'obj':
-    utils.exportOBJ('export/' + filename)
-  if type == 'pcd':
-    utils.exportPCD('export/' + filename)
+    if type == 'obj':
+        utils.exportOBJ('export/' + filename)
+    if type == 'pcd':
+        utils.exportPCD('export/' + filename)
+
 
 #click on data
 last = [0, 0, 0]
+
+
 def onclick(event):
-  width = utils.getWidth()
-  height = utils.getHeight()
-  if event.xdata is not None and event.ydata is not None:
-    x = width - int(event.ydata) - 1
-    y = height - int(event.xdata) - 1
-    if x > 1 and y > 1 and x < width - 2 and y < height - 2:
-      depth = utils.parseDepth(x, y)
-      if depth:
-        res = utils.convert2Dto3D(calibration[1], x, y, depth)
-        if res:
-          diff = [last[0] - res[0], last[1] - res[1], last[2] - res[2]]
-          dst = np.sqrt(diff[0] * diff[0] + diff[1] * diff[1] + diff[2] * diff[2])
-          res.append(dst)
-          print('x=' + str(res[0]) + ', y=' + str(res[1]) + ', depth=' + str(res[2]) + ', diff=' + str(res[3]))
-          last[0] = res[0]
-          last[1] = res[1]
-          last[2] = res[2]
-          return
-      print('no valid data')
-
-#get input
-def process(plt, dir, depth, rgb):
-
-  #extract depthmap
-  with zipfile.ZipFile(dir + '/depth/' + depth, 'r') as zip_ref:
-    zip_ref.extractall('.')
-  utils.parseData('data')
-
-  #read rgb data
-  global im_array
-  if rgb:
     width = utils.getWidth()
     height = utils.getHeight()
-    pil_im = Image.open(dir + '/rgb/' + rgb)
-    pil_im = pil_im.resize((width, height), Image.ANTIALIAS)
-    im_array = np.asarray(pil_im)
-  else:
-    im_array = 0
+    if event.xdata is not None and event.ydata is not None:
+        x = width - int(event.ydata) - 1
+        y = height - int(event.xdata) - 1
+        if x > 1 and y > 1 and x < width - 2 and y < height - 2:
+            depth = utils.parseDepth(x, y)
+            if depth:
+                res = utils.convert2Dto3D(calibration[1], x, y, depth)
+                if res:
+                    diff = [last[0] - res[0], last[1] - res[1], last[2] - res[2]]
+                    dst = np.sqrt(diff[0] * diff[0] + diff[1] * diff[1] + diff[2] * diff[2])
+                    res.append(dst)
+                    print('x=' + str(res[0]) + ', y=' + str(res[1])
+                          + ', depth=' + str(res[2]) + ', diff=' + str(res[3]))
+                    last[0] = res[0]
+                    last[1] = res[1]
+                    last[2] = res[2]
+                    return
+            print('no valid data')
 
-  #parse calibration
-  global calibration
-  calibration = utils.parseCalibration(dir + '/camera_calibration.txt')
+#get input
+
+
+def process(plt, dir, depth, rgb):
+
+    #extract depthmap
+    with zipfile.ZipFile(dir + '/depth/' + depth, 'r') as zip_ref:
+        zip_ref.extractall('.')
+    utils.parseData('data')
+
+    #read rgb data
+    global im_array
+    if rgb:
+        width = utils.getWidth()
+        height = utils.getHeight()
+        pil_im = Image.open(dir + '/rgb/' + rgb)
+        pil_im = pil_im.resize((width, height), Image.ANTIALIAS)
+        im_array = np.asarray(pil_im)
+    else:
+        im_array = 0
+
+    #parse calibration
+    global calibration
+    calibration = utils.parseCalibration(dir + '/camera_calibration.txt')
 
 #show edges
+
+
 def showEdges():
-  fig = plt.figure()
-  fig.canvas.mpl_connect('button_press_event', onclick)
+    fig = plt.figure()
+    fig.canvas.mpl_connect('button_press_event', onclick)
 
-  #calculate smoothed normalmap
-  smoothing = 3
-  maxdepth = 0
-  width = utils.getWidth()
-  height = utils.getHeight()
-  normalmap = np.zeros((width, height, 3))
-  for x in range(1 + smoothing, width - 1 - smoothing):
-    for y in range(1 + smoothing, height - 1 - smoothing):
-      maxdepth = max(maxdepth, utils.parseDepth(x, y))
-      cmx = utils.convert2Dto3D(calibration[1], x - 1, y, utils.parseDepthSmoothed(x - 1, y, smoothing))
-      cmy = utils.convert2Dto3D(calibration[1], x, y - 1, utils.parseDepthSmoothed(x, y - 1, smoothing))
-      cpx = utils.convert2Dto3D(calibration[1], x + 1, y, utils.parseDepthSmoothed(x + 1, y, smoothing))
-      cpy = utils.convert2Dto3D(calibration[1], x, y + 1, utils.parseDepthSmoothed(x, y + 1, smoothing))
-      n = utils.normalize(utils.cross(utils.sub(cpx, cmx), utils.sub(cpy, cmy)))
-      normalmap[x][height - y - 1][0] = min(abs(n[0]), 1)
-      normalmap[x][height - y - 1][1] = min(abs(n[1]), 1)
-      normalmap[x][height - y - 1][2] = min(abs(n[2]), 1)
+    #calculate smoothed normalmap
+    smoothing = 3
+    maxdepth = 0
+    width = utils.getWidth()
+    height = utils.getHeight()
+    normalmap = np.zeros((width, height, 3))
+    for x in range(1 + smoothing, width - 1 - smoothing):
+        for y in range(1 + smoothing, height - 1 - smoothing):
+            maxdepth = max(maxdepth, utils.parseDepth(x, y))
+            cmx = utils.convert2Dto3D(calibration[1], x - 1, y, utils.parseDepthSmoothed(x - 1, y, smoothing))
+            cmy = utils.convert2Dto3D(calibration[1], x, y - 1, utils.parseDepthSmoothed(x, y - 1, smoothing))
+            cpx = utils.convert2Dto3D(calibration[1], x + 1, y, utils.parseDepthSmoothed(x + 1, y, smoothing))
+            cpy = utils.convert2Dto3D(calibration[1], x, y + 1, utils.parseDepthSmoothed(x, y + 1, smoothing))
+            n = utils.normalize(utils.cross(utils.sub(cpx, cmx), utils.sub(cpy, cmy)))
+            normalmap[x][height - y - 1][0] = min(abs(n[0]), 1)
+            normalmap[x][height - y - 1][1] = min(abs(n[1]), 1)
+            normalmap[x][height - y - 1][2] = min(abs(n[2]), 1)
 
-  #fade normalmap
-  #for x in range(1, width):
-  #  for y in range(1, height):
-  #    factor = utils.parseDepth(x, y) / maxdepth
-  #    normalmap[x][height - y - 1][0] *= (1 - factor)
-  #    normalmap[x][height - y - 1][1] *= (1 - factor)
-  #    normalmap[x][height - y - 1][2] *= (1 - factor)
+    #fade normalmap
+    #for x in range(1, width):
+    #  for y in range(1, height):
+    #    factor = utils.parseDepth(x, y) / maxdepth
+    #    normalmap[x][height - y - 1][0] *= (1 - factor)
+    #    normalmap[x][height - y - 1][1] *= (1 - factor)
+    #    normalmap[x][height - y - 1][2] *= (1 - factor)
 
-  #calculate edgemap
-  edgemap = np.zeros((width, height, 3))
-  for x in range(2, width - 2):
-    for y in range(2, height - 2):
+    #calculate edgemap
+    edgemap = np.zeros((width, height, 3))
+    for x in range(2, width - 2):
+        for y in range(2, height - 2):
 
-      #calculate edge from depthmap
-      d = utils.convert2Dto3D(calibration[1], x, y, utils.parseDepth(x, y))
-      mx = utils.convert2Dto3D(calibration[1], x - 1, y, utils.parseDepth(x - 1, y))
-      my = utils.convert2Dto3D(calibration[1], x, y - 1, utils.parseDepth(x, y - 1))
-      px = utils.convert2Dto3D(calibration[1], x + 1, y, utils.parseDepth(x + 1, y))
-      py = utils.convert2Dto3D(calibration[1], x, y + 1, utils.parseDepth(x, y + 1))
-      edge = max(max(utils.length(d, mx), utils.length(d, my)), max(utils.length(d, px), utils.length(d, py)))
-      if edge > 0.1: #difference of depth in meters
-        edgemap[x][height - y - 1][1] = 1
+            #calculate edge from depthmap
+            d = utils.convert2Dto3D(calibration[1], x, y, utils.parseDepth(x, y))
+            mx = utils.convert2Dto3D(calibration[1], x - 1, y, utils.parseDepth(x - 1, y))
+            my = utils.convert2Dto3D(calibration[1], x, y - 1, utils.parseDepth(x, y - 1))
+            px = utils.convert2Dto3D(calibration[1], x + 1, y, utils.parseDepth(x + 1, y))
+            py = utils.convert2Dto3D(calibration[1], x, y + 1, utils.parseDepth(x, y + 1))
+            edge = max(max(utils.length(d, mx), utils.length(d, my)), max(utils.length(d, px), utils.length(d, py)))
+            if edge > 0.1:  # difference of depth in meters
+                edgemap[x][height - y - 1][1] = 1
 
-      #calculate edge from normalmap
-      d = normalmap[x][height - y - 1]
-      mx = normalmap[x - 1][height - y - 1]
-      my = normalmap[x][height - y - 1 - 1]
-      px = normalmap[x + 1][height - y - 1]
-      py = normalmap[x][height - y - 1 + 1]
-      edge = max(max(utils.dot(d, mx), utils.dot(d, my)), max(utils.dot(d, px), utils.dot(d, py)))
-      if edge > 0.9: #normal matching in percentage
-        edgemap[x][height - y - 1][0] = 1
+            #calculate edge from normalmap
+            d = normalmap[x][height - y - 1]
+            mx = normalmap[x - 1][height - y - 1]
+            my = normalmap[x][height - y - 1 - 1]
+            px = normalmap[x + 1][height - y - 1]
+            py = normalmap[x][height - y - 1 + 1]
+            edge = max(max(utils.dot(d, mx), utils.dot(d, my)), max(utils.dot(d, px), utils.dot(d, py)))
+            if edge > 0.9:  # normal matching in percentage
+                edgemap[x][height - y - 1][0] = 1
 
-  #calculate output
-  output = np.zeros((width, height, 3))
-  for x in range(2, width - 2):
-    for y in range(2, height - 2):
-      if edgemap[x][height - y - 1][1] == 1:
-        output[x][height - y - 1][2] = 1
-      else:
-        if edgemap[x][height - y - 1][0] == 0:
-          if edgemap[x - 1][height - y - 1][0] == 1:
-            output[x][height - y - 1][1] = 1
-          if edgemap[x][height - y - 1 - 1][0] == 1:
-            output[x][height - y - 1][1] = 1
-          if edgemap[x + 1][height - y - 1][0] == 1:
-            output[x][height - y - 1][1] = 1
-          if edgemap[x][height - y - 1 + 1][0] == 1:
-            output[x][height - y - 1][1] = 1
+    #calculate output
+    output = np.zeros((width, height, 3))
+    for x in range(2, width - 2):
+        for y in range(2, height - 2):
+            if edgemap[x][height - y - 1][1] == 1:
+                output[x][height - y - 1][2] = 1
+            else:
+                if edgemap[x][height - y - 1][0] == 0:
+                    if edgemap[x - 1][height - y - 1][0] == 1:
+                        output[x][height - y - 1][1] = 1
+                    if edgemap[x][height - y - 1 - 1][0] == 1:
+                        output[x][height - y - 1][1] = 1
+                    if edgemap[x + 1][height - y - 1][0] == 1:
+                        output[x][height - y - 1][1] = 1
+                    if edgemap[x][height - y - 1 + 1][0] == 1:
+                        output[x][height - y - 1][1] = 1
 
-  #plt.imshow(normalmap, extent=[0, height, 0, width])
-  #plt.imshow(edgemap, extent=[0, height, 0, width])
-  plt.imshow(output, extent=[0, height, 0, width])
+    #plt.imshow(normalmap, extent=[0, height, 0, width])
+    #plt.imshow(edgemap, extent=[0, height, 0, width])
+    plt.imshow(output, extent=[0, height, 0, width])
 
 #show result
-def showResult():
-  fig = plt.figure()
-  fig.canvas.mpl_connect('button_press_event', onclick)
-  width = utils.getWidth()
-  height = utils.getHeight()
-  output = np.zeros((width, height, 3))
-  for x in range(width):
-    for y in range(height):
-      depth = utils.parseDepth(x, y)
-      if (depth):
-        #convert ToF coordinates into RGB coordinates
-        vec = utils.convert2Dto3D(calibration[1], x, y, depth)
-        vec[0] += calibration[2][0]
-        vec[1] += calibration[2][1]
-        vec[2] += calibration[2][2]
-        vec = utils.convert3Dto2D(calibration[0], vec[0], vec[1], vec[2])
 
-        #set output pixel
-        output[x][height - y - 1][0] = utils.parseConfidence(x, y)
-        if im_array and vec[0] > 0 and vec[1] > 1 and vec[0] < width and vec[1] < height:
-          output[x][height - y - 1][1] = im_array[vec[1]][vec[0]][1] / 255.0 #test matching on RGB data
-        output[x][height - y - 1][2] = 1.0 - min(depth / 2.0, 1.0) #depth data scaled to be visible
-  plt.imshow(output, extent=[0, height, 0, width])
+
+def showResult():
+    fig = plt.figure()
+    fig.canvas.mpl_connect('button_press_event', onclick)
+    width = utils.getWidth()
+    height = utils.getHeight()
+    output = np.zeros((width, height, 3))
+    for x in range(width):
+        for y in range(height):
+            depth = utils.parseDepth(x, y)
+            if (depth):
+                #convert ToF coordinates into RGB coordinates
+                vec = utils.convert2Dto3D(calibration[1], x, y, depth)
+                vec[0] += calibration[2][0]
+                vec[1] += calibration[2][1]
+                vec[2] += calibration[2][2]
+                vec = utils.convert3Dto2D(calibration[0], vec[0], vec[1], vec[2])
+
+                #set output pixel
+                output[x][height - y - 1][0] = utils.parseConfidence(x, y)
+                if im_array and vec[0] > 0 and vec[1] > 1 and vec[0] < width and vec[1] < height:
+                    output[x][height - y - 1][1] = im_array[vec[1]][vec[0]][1] / 255.0  # test matching on RGB data
+                output[x][height - y - 1][2] = 1.0 - min(depth / 2.0, 1.0)  # depth data scaled to be visible
+    plt.imshow(output, extent=[0, height, 0, width])
--- src/common/depthmap_toolkit/toolkit.py
+++ src/common/depthmap_toolkit/toolkit.py
@@ -5,89 +5,101 @@ from shutil import copyfile
 import depthmap
 import pcd2depth
 import shutil
-import os, sys
+import os
+import sys
+
 
 def convertAllPCDs(event):
-  input = 'export'
-  pcd = []
-  for (dirpath, dirnames, filenames) in walk(input):
-    pcd = filenames
-  pcd.sort()
-  try:
-    shutil.rmtree('output')
-  except:
-    print('no previous data to delete')
-  os.mkdir('output');
-  os.mkdir('output/depth');
-  copyfile(input + '/../camera_calibration.txt', 'output/camera_calibration.txt')
-  for i in range(len(pcd)):
-    pcd2depth.process(input + '/../camera_calibration.txt', input + '/' + pcd[i], 'output/depth/' + pcd[i] + '.depth')
-  print 'Data exported into folder output'
+    input = 'export'
+    pcd = []
+    for (dirpath, dirnames, filenames) in walk(input):
+        pcd = filenames
+    pcd.sort()
+    try:
+        shutil.rmtree('output')
+    except BaseException:
+        print('no previous data to delete')
+    os.mkdir('output')
+    os.mkdir('output/depth')
+    copyfile(input + '/../camera_calibration.txt', 'output/camera_calibration.txt')
+    for i in range(len(pcd)):
+        pcd2depth.process(
+            input + '/../camera_calibration.txt',
+            input + '/' + pcd[i],
+            'output/depth/' + pcd[i] + '.depth')
+    print('Data exported into folder output')
+
 
 def exportOBJ(event):
-  depthmap.export('obj', 'output' + str(index) + '.obj')
+    depthmap.export('obj', 'output' + str(index) + '.obj')
+
 
 def exportPCD(event):
-  depthmap.export('pcd', 'output' + str(index) + '.pcd')
+    depthmap.export('pcd', 'output' + str(index) + '.pcd')
+
 
 def next(event):
-  plt.close()
-  global index
-  index = index + 1
-  if (index == size):
-    index = 0
-  show()
+    plt.close()
+    global index
+    index = index + 1
+    if (index == size):
+        index = 0
+    show()
+
 
 def prev(event):
-  plt.close()
-  global index
-  index = index - 1
-  if (index == -1):
-    index = size - 1
-  show()
-  
+    plt.close()
+    global index
+    index = index - 1
+    if (index == -1):
+        index = size - 1
+    show()
+
+
 def swapEdges(event):
-  plt.close()
-  global edges
-  edges = 1 - edges
-  show()
+    plt.close()
+    global edges
+    edges = 1 - edges
+    show()
+
 
 def show():
-  depthmap.process(plt, input, depth[index], 0)#rgb[index])
-  if edges == 1:
-    depthmap.showEdges()
-  else:
-    depthmap.showResult()
-  ax = plt.gca()
-  ax.text(0.5, 1.075, depth[index], horizontalalignment='center',verticalalignment='center', transform=ax.transAxes)
-  bprev = Button(plt.axes([0.0, 0.0, 0.1, 0.075]), '<<', color='gray')
-  bprev.on_clicked(prev)
-  bnext = Button(plt.axes([0.9, 0.0, 0.1, 0.075]), '>>', color='gray')
-  bnext.on_clicked(next)
-  bexportOBJ = Button(plt.axes([0.2, 0.0, 0.2, 0.05]), 'Export OBJ', color='gray')
-  bexportOBJ.on_clicked(exportOBJ)
-  bexportPCD = Button(plt.axes([0.4, 0.0, 0.2, 0.05]), 'Export PCD', color='gray')
-  bexportPCD.on_clicked(exportPCD)
-  bconvertPCDs = Button(plt.axes([0.6, 0.0, 0.2, 0.05]), 'Convert all PCDs', color='gray')
-  bconvertPCDs.on_clicked(convertAllPCDs)
-  if edges == 0:
-    bshowedges = Button(plt.axes([0.0, 0.94, 0.2, 0.05]), 'Show edges', color='gray')
-    bshowedges.on_clicked(swapEdges)
-  else:
-    bshowedges = Button(plt.axes([0.0, 0.94, 0.2, 0.05]), 'Hide edges', color='gray')
-    bshowedges.on_clicked(swapEdges)
-  plt.show()
+    depthmap.process(plt, input, depth[index], 0)  # rgb[index])
+    if edges == 1:
+        depthmap.showEdges()
+    else:
+        depthmap.showResult()
+    ax = plt.gca()
+    ax.text(0.5, 1.075, depth[index], horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
+    bprev = Button(plt.axes([0.0, 0.0, 0.1, 0.075]), '<<', color='gray')
+    bprev.on_clicked(prev)
+    bnext = Button(plt.axes([0.9, 0.0, 0.1, 0.075]), '>>', color='gray')
+    bnext.on_clicked(next)
+    bexportOBJ = Button(plt.axes([0.2, 0.0, 0.2, 0.05]), 'Export OBJ', color='gray')
+    bexportOBJ.on_clicked(exportOBJ)
+    bexportPCD = Button(plt.axes([0.4, 0.0, 0.2, 0.05]), 'Export PCD', color='gray')
+    bexportPCD.on_clicked(exportPCD)
+    bconvertPCDs = Button(plt.axes([0.6, 0.0, 0.2, 0.05]), 'Convert all PCDs', color='gray')
+    bconvertPCDs.on_clicked(convertAllPCDs)
+    if edges == 0:
+        bshowedges = Button(plt.axes([0.0, 0.94, 0.2, 0.05]), 'Show edges', color='gray')
+        bshowedges.on_clicked(swapEdges)
+    else:
+        bshowedges = Button(plt.axes([0.0, 0.94, 0.2, 0.05]), 'Hide edges', color='gray')
+        bshowedges.on_clicked(swapEdges)
+    plt.show()
+
 
 #prepare
 if len(sys.argv) != 2:
-  print('You did not enter input folder')
-  print('E.g.: python toolkit.py honor')
-  sys.exit(1)
+    print('You did not enter input folder')
+    print('E.g.: python toolkit.py honor')
+    sys.exit(1)
 input = sys.argv[1]
 depth = []
 #rgb = []
 for (dirpath, dirnames, filenames) in walk(input + '/depth'):
-  depth = filenames
+    depth = filenames
 #for (dirpath, dirnames, filenames) in walk(input + '/rgb'):
 #  rgb = filenames
 depth.sort()
@@ -95,14 +107,13 @@ depth.sort()
 
 #make sure there is a new export folder
 try:
-  shutil.rmtree('export')
-except:
-  print('no previous data to delete')
-os.mkdir('export');
+    shutil.rmtree('export')
+except BaseException:
+    print('no previous data to delete')
+os.mkdir('export')
 
 #show viewer
 edges = 0
 index = 0
 size = len(depth)
 show()
-
