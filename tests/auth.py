ec5932b7581fffe677bd07b2a7c8111b794c32ba
gangooteli <24511086+gangooteli@users.noreply.github.com>
2020-10-07 22:28:00 +0530

    Adding Service Principal Auth

--- tests/auth.py
+++ tests/auth.py
@@ -1,18 +1,41 @@
-import argparse 
+import argparse
+import logging
 from azureml.core import Workspace
-
+from azureml.core.authentication import ServicePrincipalAuthentication
 
 # Initialize parser 
 parser = argparse.ArgumentParser() 
+parser.add_argument("-tid", "--tenant_id", help = "Tenant Id")
+parser.add_argument("-spid", "--service_principal_id", help = "Service Principal Id")
+parser.add_argument("-sppwd", "--service_principal_password", help = "Service Principal Password")
+
 parser.add_argument("-sid", "--subscription_id", help = "Subscription ID")
 parser.add_argument("-rg", "--resource_group", help = "Resource Group")
 parser.add_argument("-wn", "--workspace_name", help = "Workspace  Name")
+
+
 args = parser.parse_args()
 
+print("Trying to create Workspace with CLI Authentication")
 
-ws = Workspace(subscription_id=args.subscription_id, 
-            resource_group=args.resource_group, 
-            workspace_name= args.workspace_name)
+svc_pr = ServicePrincipalAuthentication(
+    tenant_id = args.tenant_id,
+    service_principal_id = args.service_principal_id,
+    service_principal_password = args.service_principal_password)
+
+print("Get auth Header")
+
+print(svc_pr.get_authentication_header())
+
+print("Login Successful")
+
+
+ws = Workspace(subscription_id = args.subscription_id, 
+    resource_group = args.resource_group, 
+    workspace_name = args.workspace_name)
+
+
+
 
 
 print("Workspace Details")
