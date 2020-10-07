b594c3dbe6a5d9b9f9ef8346ad49b0ef88cd2286
gangooteli <24511086+gangooteli@users.noreply.github.com>
2020-10-07 23:54:01 +0530

    Added login using CLI auth

--- tests/auth.py
+++ tests/auth.py
@@ -1,13 +1,26 @@
 import argparse
 import logging
 from azureml.core import Workspace
-from azureml.core.authentication import ServicePrincipalAuthentication
+#from azureml.core.authentication import ServicePrincipalAuthentication
+from azureml.core.authentication import AuthenticationException, AzureCliAuthentication
 
+def get_auth():
+    logger = logging.getLogger(__name__)
+    logger.debug("Trying to create Workspace with CLI Authentication")
+    try:
+        auth = AzureCliAuthentication()
+        auth.get_authentication_header()
+    except AuthenticationException:
+        print("Auth Error")
+    return auth
+
+
+'''
 # Initialize parser 
 parser = argparse.ArgumentParser() 
-parser.add_argument("-tid", "--tenant_id", help = "Tenant Id")
-parser.add_argument("-spid", "--service_principal_id", help = "Service Principal Id")
-parser.add_argument("-sppwd", "--service_principal_password", help = "Service Principal Password")
+#parser.add_argument("-tid", "--tenant_id", help = "Tenant Id")
+#parser.add_argument("-spid", "--service_principal_id", help = "Service Principal Id")
+#parser.add_argument("-sppwd", "--service_principal_password", help = "Service Principal Password")
 
 parser.add_argument("-sid", "--subscription_id", help = "Subscription ID")
 parser.add_argument("-rg", "--resource_group", help = "Resource Group")
@@ -15,6 +28,7 @@ parser.add_argument("-wn", "--workspace_name", help = "Workspace  Name")
 
 
 args = parser.parse_args()
+
 
 print("Trying to create Workspace with CLI Authentication")
 
@@ -57,3 +71,19 @@ print("Workspace Details")
 print(ws.get_details())
 
 print("Auth One End")
+
+'''
+
+print("CLI Auth started")
+auth =  get_auth()
+print("CLI Auth success")
+
+print("Trying to create Workspace with CLI Authentication")
+print("Workspace access from config using CLI Auth")
+ws = Workspace.from_config(auth=get_auth())
+print("Workspace access from config successful")
+
+print("Workspace Details")
+print(ws.get_details())
+
+print("Auth End")
